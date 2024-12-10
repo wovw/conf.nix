a60f8f32dccfc59b3a943efd2f96b3bedc8edf16
wovw <anthonypasala12@gmail.com>
2024-12-10 17:30:06 -0600

    feat(nvim): add telescope multigrep

--- config/nvim/lua/wovw/lazy/telescope.lua
+++ config/nvim/lua/wovw/lazy/telescope.lua
@@ -3,19 +3,22 @@ return {
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+        { 'nvim-tree/nvim-web-devicons',              enabled = vim.g.have_nerd_font },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-        require('telescope').setup({
+        local telescope = require('telescope')
+        telescope.setup({
             extensions = {
                 fzf = {
                     fuzzy = true,                   -- false will only do exact matching
                     override_generic_sorter = true, -- override the generic sorter
                     override_file_sorter = true,    -- override the file sorter
                     case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-                }
+                },
             }
         })
+        telescope.load_extension('fzf')
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -31,5 +34,7 @@ return {
         vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>pk', builtin.keymaps, {})
+
+        require('wovw.lazy.telescope.multigrep').setup()
     end
 }
