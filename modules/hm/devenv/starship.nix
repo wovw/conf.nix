b6a560a9652aaebe92c522b06c5d148146d4f6c5
wovw <anthonypasala12@gmail.com>
2025-07-17 21:52:03 -0400

    chore: nix flake update
    fix: use default starship arrow

--- modules/hm/devenv/starship.nix
+++ modules/hm/devenv/starship.nix
@@ -4,13 +4,6 @@
     enable = true;
     settings = {
       command_timeout = 10000;
-      character = {
-        success_symbol = "[󰅂](bold green)";
-        error_symbol = "[󰅂](bold red)";
-        vimcmd_symbol = "[󰅁](bold green)";
-        vimcmd_replace_one_symbol = "[󰅁](bold purple)";
-        vimcmd_replace_symbol = "[󰅁](bold yellow)";
-      };
       aws = {
         format = "\\[[$symbol($profile)(\\($region\\))(\\[$duration\\])]($style)\\]";
       };
