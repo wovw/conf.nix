cddb2672eb001ff593aed660a794b3ff3dd82939
wovw <anthonypasala12@gmail.com>
2025-07-23 10:02:35 -0400

    fix: bluetooth issues maybe

--- modules/de/hyprland/system.nix
+++ modules/de/hyprland/system.nix
@@ -53,6 +53,17 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+      # @see https://discourse.nixos.org/t/bluetooh-not-working/48430/3
+      wireplumber.configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+          monitor.bluez.properties = {
+            bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+            bluez5.codecs = [ sbc sbc_xq aac ]
+            bluez5.enable-sbc-xq = true
+            bluez5.hfphsp-backend = "native"
+          }
+        '')
+      ];
     };
   };
   systemd = {
