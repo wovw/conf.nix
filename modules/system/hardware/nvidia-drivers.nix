08d9afa29286f252bc206098c0cd7d13c791ec87
wovw <anthonypasala12@gmail.com>
2025-12-14 01:16:13 -0500

    nvidia stable compiles with latest cachyos kernel

--- modules/system/hardware/nvidia-drivers.nix
+++ modules/system/hardware/nvidia-drivers.nix
@@ -53,7 +53,7 @@ in
       # accessible via `nvidia-settings`.
       nvidiaSettings = true;
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
     nixpkgs.config.packageOverrides = pkgs: {
